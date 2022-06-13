725b3959fe668350fe368a9334033e4175e25ecd
rocean <rocean74@gmail.com>
2022-06-13 21:42:39 +0300

    Service system

---
+++ src/Service/ServiceSystem.java
@@ -0,0 +1,80 @@
+package Service;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Yiannis Kiranis <yiannis.kiranis@gmail.com>
+ * https://apps4net.eu
+ * Date: 13/6/22
+ * Time: 8:51 μ.μ.
+ *
+ * Άσκηση 2, Τελικές 2018
+ *
+ */
+
+abstract class Service {
+    private ArrayList<Task> tasks = new ArrayList<>();
+    private ArrayList<Order> orders = new ArrayList<>();
+
+    abstract double calcWorksCost(double hoursOrSpace);
+}
+
+class BuildingService extends Service {
+
+    @Override
+    double calcWorksCost(double hoursOrSpace) {
+        return 0;
+    }
+}
+
+class GardenService extends Service {
+
+    @Override
+    double calcWorksCost(double hoursOrSpace) {
+        return 0;
+    }
+}
+
+class Task {
+
+}
+
+class Order {
+    private ArrayList<Task> tasks = new ArrayList<>();
+    private Customer customer;
+    private Payment payment;
+}
+
+class Customer {
+    private ArrayList<Building> buildings = new ArrayList<>();
+}
+
+class Payment {
+
+}
+
+class Building {
+
+}
+
+class Apartment  extends Building {
+
+}
+
+class House extends Building {
+    private Garden garden;
+}
+
+class Garden {
+
+}
+
+class Company {
+    private ArrayList<Order> orders = new ArrayList<>();
+}
+
+public class ServiceSystem {
+    public static void main(String[] args) {
+
+    }
+}
