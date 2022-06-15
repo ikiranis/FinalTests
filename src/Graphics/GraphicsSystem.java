f1b0456d2ac17b69e7eca2153b1825cc6302061e
rocean <rocean74@gmail.com>
2022-06-15 05:15:50 +0300

    Changes

--- src/Graphics/GraphicsSystem.java
+++ src/Graphics/GraphicsSystem.java
@@ -1,5 +1,8 @@
 package Graphics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 abstract class GraphicObject {
     private String name;
     private double[] position = new double[2];
@@ -41,5 +44,57 @@ class Circle extends GraphicObject {
     }
 }
 
+class Group extends GraphicObject {
+    private List<GraphicObject> elements;
+
+    public Group(String name, int x, int y, List<GraphicObject> elements) throws Exception {
+        super(name, x, y);
+
+        if (elements.size() < 2) {
+            throw new Exception("H ομάδα θα πρέπει να περιλαμβάνει 2 ή περισσότερα αντικείμενα");
+        } else {
+            this.elements = elements;
+        }
+    }
+
+    public void addElement(GraphicObject element) {
+        elements.add(element);
+    }
+
+    public void removeElement(GraphicObject element) {
+        elements.remove(element);
+    }
+
+    @Override
+    public double emvadon() {
+        double emvadon = 0;
+
+        for (GraphicObject g : elements) {
+            emvadon += g.emvadon();
+        }
+
+        return emvadon;
+    }
+}
+
 public class GraphicsSystem {
+    public static void main(String[] args) {
+        List<GraphicObject> elements = new ArrayList<>();
+
+        Circle c1 = new Circle("Circle 1", 3, 4, 5);
+        Circle c2 = new Circle("Circle 2", 3, 4, 7);
+        Square s1 = new Square("Square 1", 10, 12, 10);
+        Square s2 = new Square("Square 2", 10, 12, 20);
+
+        elements.add(c1);
+        elements.add(s1);
+
+        try {
+            Group group = new Group("Group 1", 7, 8, elements);
+
+            System.out.println("Συνολικό εμβαδόν: " + group.emvadon());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
